49243b55a36c378e41a01389f49a62d95656985b
rama2698 <r.bhagat2698@gmail.com>
2024-08-10 11:43:50 +0530

    Created views for home

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.test import Client
 
-# Create your views here.
+def myPortfolio(request):
+    client = Client()
+    portfolioApiUrl = reverse('getAllPortfolioData')
+    portfolioResponse = client.get(portfolioApiUrl, {"device": request.device_type}).json()
+    portfolioResponse['device'] = request.device_type
+    return render(request, 'portfolio/myPortfolio.html', portfolioResponse)
